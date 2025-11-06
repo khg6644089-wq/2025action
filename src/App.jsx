e6190ad50de70ab5139e8fdb19777c2481d512ba
hoons <khg6644089@hanmail.net>
2025-11-06 17:47:51 +0900

    action#7

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ function App() {
     <div>
       <h3>title</h3>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, error!
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima optio
+        beatae natus quos, laborum doloremque quam veniam voluptatibus eveniet
+        ut, adipisci assumenda modi perferendis vero, hic amet dolorem ab
+        cupiditate? Quia, quas totam dolor corrupti animi officia atque earum
+        nostrum praesentium ipsum perspiciatis! Incidunt aut, laborum dolore vel
+        tempora error itaque totam repellat distinctio facilis ullam temporibus
+        earum esse fuga. Repellendus alias accusamus ad laudantium quidem quas
+        illo autem quam culpa omnis similique id minus, ratione impedit deleniti
+        magnam unde quisquam amet, maxime aliquid neque placeat laborum cumque
+        maiores! Distinctio necessitatibus nisi quae nobis error sed autem
+        facilis ullam voluptate.
+      </p>
+      <p>오늘수업끝!!!!!</p>
     </div>
   );
 }
